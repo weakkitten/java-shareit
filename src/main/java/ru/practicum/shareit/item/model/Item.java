d40ceca1e3b6d6067b3151a85a561139fc0bb400
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-05-02 13:10:01 +0400

    Add files via upload

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.Positive;
+
+@Data
+@Builder
 public class Item {
+    @Positive
+    private int id;
+    private String name;
+    private String description;
+    private boolean available;
+    @Positive
+    private int owner;
+    @Positive
+    private int request;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.user;
 
+import org.hibernate.validator.constraints.UniqueElements;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Positive;
+
 /**
  * TODO Sprint add-controllers.
  */
 public class User {
+    @Positive
+    private int id;
+    private String name;
+    @Email
+    @UniqueElements
+    private String email;
 }
