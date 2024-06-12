cdc03abdbe8a8fd1b9cee0b45ae65d904112b430
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-06-12 20:10:48 +0400

    Add files via upload

--- src/test/java/ru/practicum/shareit/request/repository/RequestRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/request/repository/RequestRepositoryTest.java
@@ -5,15 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RequestRepositoryTest {
-
-    @Test
-    void findByDescriptionAndRequesterId() {
-    }
-
-    @Test
-    void findByRequesterIdOrderByCreatedDesc() {
-    }
-
     @Test
     void findByNotRequesterId() {
     }
