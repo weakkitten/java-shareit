676eebd3f766cdfd40a41ce15a06a721fd687649
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-06-12 20:01:02 +0400

    Add files via upload

---
+++ src/test/java/ru/practicum/shareit/request/repository/RequestRepositoryTest.java
@@ -0,0 +1,20 @@
+package ru.practicum.shareit.request.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestRepositoryTest {
+
+    @Test
+    void findByDescriptionAndRequesterId() {
+    }
+
+    @Test
+    void findByRequesterIdOrderByCreatedDesc() {
+    }
+
+    @Test
+    void findByNotRequesterId() {
+    }
+}
