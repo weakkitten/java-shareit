0dadc56450dcdcabf291d5be915b64bfbe74ede3
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-06-12 18:46:06 +0400

    Add files via upload

---
+++ src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -0,0 +1,32 @@
+package ru.practicum.shareit.item;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemControllerTest {
+
+    @Test
+    void createItem() {
+    }
+
+    @Test
+    void updateItem() {
+    }
+
+    @Test
+    void getItem() {
+    }
+
+    @Test
+    void getAllItemByUser() {
+    }
+
+    @Test
+    void searchItem() {
+    }
+
+    @Test
+    void createComment() {
+    }
+}
---
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void removeUser() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+}
