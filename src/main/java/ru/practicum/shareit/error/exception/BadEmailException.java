8e4407a321b27741c48fb0a0564450432b8bcb7c
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-06-13 09:54:27 +0400

    Delete src/main/java/ru/practicum/shareit/error/exception/BadEmailException.java

--- src/main/java/ru/practicum/shareit/error/exception/BadEmailException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.error.exception;
-
-public class BadEmailException extends RuntimeException {
-    public BadEmailException(String message) {
-        super(message);
-    }
-}
