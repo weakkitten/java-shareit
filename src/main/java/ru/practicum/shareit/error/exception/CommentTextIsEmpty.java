b5f934042fe1d0cebce8ad772c427cc736cdcbf9
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-06-13 15:04:01 +0400

    Delete src directory

--- src/main/java/ru/practicum/shareit/error/exception/CommentTextIsEmpty.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.error.exception;
-
-public class CommentTextIsEmpty extends RuntimeException {
-    public CommentTextIsEmpty(String message) {
-        super(message);
-    }
-}
