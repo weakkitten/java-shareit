6f9c24ab929a5d0849bd252bcff3baca1d53dc72
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-06-20 21:22:10 +0400

    Add files via upload

---
+++ gateway/src/main/java/ru/practicum/shareit/error/exception/UnsupportedState.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.error.exception;
+
+public class UnsupportedState extends RuntimeException {
+    public UnsupportedState(String message) {
+        super(message);
+    }
+}
