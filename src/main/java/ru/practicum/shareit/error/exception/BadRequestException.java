c7793a4f7e4d95fb4f5de4161476e21c884c18e2
weakkitten <146001313+weakkitten@users.noreply.github.com>
2024-06-20 11:13:13 +0400

    Delete src directory

--- src/main/java/ru/practicum/shareit/error/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.error.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
